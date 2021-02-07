93a861b3ebe9e6317521b2baac9294ddd1d4f413
han-tyumi <mmchamp95@gmail.com>
2021-02-07 12:35:07 -0500

    docs: :page_facing_up: add GPLv3 license

--- table/format.go
+++ table/format.go
@@ -1,3 +1,20 @@
+/*
+Copyright © 2021 Matthew Champagne <mmchamp95@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package table
 
 import (
@@ -52,6 +69,7 @@ var tokenFormatter = map[string]func(*mcf.Mod) (value string){
 type Format string
 
 // Headers returns the table header names for the Format.
+// TODO: Allow this function to take in a token map to make this more generic.
 func (f *Format) Headers() (headers []string) {
 	var token, header string
 
@@ -93,6 +111,7 @@ func (f *Format) Headers() (headers []string) {
 }
 
 // Values returns the table values for a given mod and Format.
+// TODO: Allow this function to take in a token map to make this more generic.
 func (f *Format) Values(mod *mcf.Mod) (values []string) {
 	var token, value string
 
